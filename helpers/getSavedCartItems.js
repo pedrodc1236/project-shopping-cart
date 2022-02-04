fec6edb0ffaf11e144ad019e8280785c5da83440
Pedro Tavares <pedrodc1236@gmail.com>
2022-02-04 17:36:23 -0300

    requisito 10 e 11 completos

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,4 +1,10 @@
-const getSavedCartItems = () => JSON.parse(localStorage.getItem('cartItems'));
+const getSavedCartItems = () => {
+  try {
+    return JSON.parse(localStorage.getItem('cartItems'));
+  } catch (error) {
+    return error;
+  }
+};
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
